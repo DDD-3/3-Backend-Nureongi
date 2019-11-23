5604b3254d598960f8c752dd7536beb613d26abe
hodongman <jhd9206@gmail.com>
2019-11-23 16:20:52 +0900

    transaction summit

--- src/models/account.go
+++ src/models/account.go
@@ -1,13 +1,12 @@
 package models
 
 import (
-	"strings"
 	"errors"
-	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/jinzhu/gorm"
 	"golang.org/x/crypto/bcrypt"
-	"github.com/dgrijalva/jwt-go"
-	utils "../utils"
 )
 
 type AuthToken struct {
@@ -17,56 +16,98 @@ type AuthToken struct {
 
 type UserAccount struct {
 	gorm.Model
-	email string `json:"email"`
+	email    string `json:"email"`
 	password string `json:"password"`
-	name string `json:"password"`
-	token string `json:"token";sql:"-"`
+	name     string `json:"password"`
+	token    string `json:"token";sql:"-"`
 }
 
 /*
 	여기서 부터는 DB Procedure 정의 입니다.
 */
 
-func ( account *UserAccount ) validateNotDBXXX() ( /*map[string] interface{},*/ error ) { // DB 검증 없는 버전
-	
-	if false == strings.Contains( account.email, "@" ) {
+func (account *UserAccount) validateNotDBXXX() /*map[string] interface{},*/ error { // DB 검증 없는 버전
+
+	if false == strings.Contains(account.email, "@") {
 		//return utils.makeHttpMessage( false, "Email address is required" ), false
-		return errors.New( "eErrorInvalidEmail" )
+		return errors.New("eErrorInvalidEmail")
 	}
 
-	if len( account.password ) < 6 {
-		return errors.New( "eErrorInvalidPasswordLength" )
+	if len(account.password) < 6 {
+		return errors.New("eErrorInvalidPasswordLength")
 	}
 
-	if len( account.name ) < 2 {
-		return errors.New( "eErrorInvalidNameLength" )
+	if len(account.name) < 2 {
+		return errors.New("eErrorInvalidNameLength")
 	}
 
 	return nil
 }
 
-func ( account *UserAccount ) validate() ( /*map[string] interface{},*/ error ) {
-	
+func (account *UserAccount) validate() /*map[string] interface{},*/ error {
+
 	err := account.validateNotDBXXX()
-	if ( nil != err ) {
+	if nil != err {
 		return err
 	}
 
 	tempAccount := &UserAccount{}
 
-	err = GetDB().Table( "accounts" ).Where( "email = ? AND name = ?" , account.email, account.name ).First( tempAccount ).Error
-	if ( nil == err && err == gorm.ErrRecordNotFound ) {
-		return errors.New( "eErrorInvalidEmailAndUserName" )
+	err = getDB().Table("accounts").Where("email = ? AND name = ?", account.email, account.name).First(tempAccount).Error
+	if nil == err && err == gorm.ErrRecordNotFound {
+		return errors.New("eErrorInvalidEmailAndUserName")
 	}
 
-	if ( "" == tempAccount.email ) {
-
+	if "" == tempAccount.email {
+		return errors.New("eErrorInvalidEmailAndUserName")
 	}
 
 	return nil
 }
 
-func ( account *UserAccount ) createAccount() ( error, /*map[string] interface{}*/ ) {
+func (account *UserAccount) createAccount() error /*map[string] interface{}*/ {
+
+	// data validate
+	if errors := account.validateNotDBXXX(); nil != errors {
+		return errors
+	}
+
+	// data setting
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(account.password), bcrypt.DefaultCost)
+	account.password = string(hashedPassword)
+
+	// Transaction Begin
+	tx := getDB().Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		return err
+	}
+
+	tempAccount := &UserAccount{}
+
+	if err := getDB().Table("accounts").Where("email = ? AND name = ?", account.email, account.name).First(tempAccount).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := getDB().Create(account).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
+	// end of transaction
+
+	if account.ID <= 0 {
+		return errors.New("DBCreateError")
+	}
+
+	autoToken := &AuthToken{userId: account.ID}
 
 	return nil
 }
